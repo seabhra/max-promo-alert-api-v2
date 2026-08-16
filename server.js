42a9faa74676626f2ae34e410b66e7c677be8001
Geraldo@NewsGames <seabhra@gmail.com>
2026-08-16 14:34:52 -0300

    fix: server.js serve arquivos estaticos e index.html na raiz

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/status', (req, res) => {
   res.json({ message: 'API' });
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
